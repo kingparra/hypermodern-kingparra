29d94665e59f8da32eb0eec80d0d03ab4c0600f3
Chris King-Parra <chris@kingparra.work>
2024-02-14 18:45:08 -0500

    Add nox

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,9 @@
 import nox
 
 
-@nox.session(python=["3.12", "3.7", "3.8"])
+# if you get errors about _sqlite3, you forgot to add sqlite-devel
+# with your system package manager before compiling python with pyenv
+@nox.session(python=["3.8", "3.12"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
